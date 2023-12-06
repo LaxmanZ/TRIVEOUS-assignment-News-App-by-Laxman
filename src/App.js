0a99e6b6903f0156351402a42d06edea84e520da
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-12-06 10:40:43 +0530

    created navbar, footer & News Component using API

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import News from './components/news/News'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <News/>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './Navbar.css';
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-4">
+        <div className="container-fluid flex flex-between">
+          <a className="navbar-brand" href="/">
+            <img
+              src="/logo-white.png"
+              style={{ width: '150px', height: '70px' }}
+              alt="logo"
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  News
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="">
+            <button className='px-4 py-2 br-2 rounded-pill bg-primary'>Login</button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
